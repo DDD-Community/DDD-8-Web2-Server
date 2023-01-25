52b6b77dfd459468471a3c4c73753b35478b52d0
jihwan <sgh002400@naver.com>
2023-01-25 22:53:57 +0900

    feat(UserController): 회원탈퇴 로직 완료

--- src/main/java/ddd/caffeine/ratrip/module/user/presentation/UserController.java
+++ src/main/java/ddd/caffeine/ratrip/module/user/presentation/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import ddd.caffeine.ratrip.module.user.domain.User;
 import ddd.caffeine.ratrip.module.user.presentation.dto.request.UpdateUserNameRequestDto;
 import ddd.caffeine.ratrip.module.user.presentation.dto.response.UserNameResponseDto;
 import ddd.caffeine.ratrip.module.user.presentation.dto.response.UserNameUpdateResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
@@ -24,16 +26,25 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	private final UserService userService;
 
+	@Operation(summary = "회원 이름 조회")
 	@GetMapping("/name")
 	public ResponseEntity<UserNameResponseDto> findUserName(
 		@Parameter(hidden = true) @AuthenticationPrincipal User user) {
 		return ResponseEntity.ok(userService.findUserName(user));
 	}
 
+	@Operation(summary = "회원 이름 변경")
 	@PatchMapping("/name")
 	public ResponseEntity<UserNameUpdateResponseDto> updateUserName(
 		@Parameter(hidden = true) @AuthenticationPrincipal User user,
 		@Valid @RequestBody UpdateUserNameRequestDto request) {
 		return ResponseEntity.ok(userService.updateName(user, request.toServiceDto()));
+	}
+
+	@Operation(summary = "회원 탈퇴")
+	@DeleteMapping("/withdrawal")
+	public ResponseEntity<String> withdrawal(@Parameter(hidden = true) @AuthenticationPrincipal User user) {
+		userService.withdrawal(user);
+		return ResponseEntity.ok("Delete Success");
 	}
 }
