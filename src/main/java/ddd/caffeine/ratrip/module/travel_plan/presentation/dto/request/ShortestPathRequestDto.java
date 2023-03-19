e7d9d4293d70499ec19969be4e308d2b789a7d2c
jihwan <sgh002400@naver.com>
2023-03-19 16:02:33 +0900

    feat(ShortestPathDto): Dto 수정

--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/request/ShortestPathRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/request/ShortestPathRequestDto.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import javax.validation.constraints.NotBlank;
 
+import ddd.caffeine.ratrip.module.travel_plan.application.dto.ShortestPathDto;
+import ddd.caffeine.ratrip.module.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,5 +17,9 @@ public class ShortestPathRequestDto {
 
 	@Schema(description = "최단 거리의 기준이 되는 장소의 ID", example = "11edb281-8a52-2d4f-9465-1970980a80db")
 	@NotBlank(message = "PlaceId must not be blank")
-	private UUID id;
+	private UUID placeId;
+
+	public ShortestPathDto toServiceDto(final User user, final UUID travelPlanId, final UUID dayScheduleId) {
+		return ShortestPathDto.of(user, travelPlanId, dayScheduleId, placeId);
+	}
 }
--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/response/ShortestPathResponseDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/response/ShortestPathResponseDto.java
@@ -1,5 +1,7 @@
 package ddd.caffeine.ratrip.module.travel_plan.presentation.dto.response;
 
+import java.util.List;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,4 +9,9 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class ShortestPathResponseDto {
+	private List<PlaceDistanceResponse> placeDistanceResponses;
+
+	public static ShortestPathResponseDto of(final List<PlaceDistanceResponse> placeDistanceResponses) {
+		return new ShortestPathResponseDto(placeDistanceResponses);
+	}
 }
