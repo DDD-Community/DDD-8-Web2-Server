ed6e4a20f3ab1c3b6d4f024096686996e6337903
jihwan <sgh002400@naver.com>
2023-04-04 18:32:25 +0900

    refactor(Blog): Blog 엔티티 초기화

--- src/main/java/ddd/caffeine/ratrip/module/blog/domain/Blog.java
+++ src/main/java/ddd/caffeine/ratrip/module/blog/domain/Blog.java
@@ -1,4 +1,28 @@
 package ddd.caffeine.ratrip.module.blog.domain;
 
-public class Blog {
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import ddd.caffeine.ratrip.common.jpa.AuditingTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Blog extends AuditingTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private UUID id;
+
+	@Column(columnDefinition = "VARCHAR(255)")
+	private String title;
+
+	@Column(columnDefinition = "VARCHAR(255)")
+	private String link;
 }
