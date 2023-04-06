4143b1e2b0b04e55ce318275e683509f4dfb9cc8
jihwan <sgh002400@naver.com>
2023-04-06 15:37:36 +0900

    리팩토링

--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/TravelPlanController.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/TravelPlanController.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +24,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("v1/travel-plans")
--- src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/request/CreateTravelPlanRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/travel_plan/presentation/dto/request/CreateTravelPlanRequestDto.java
@@ -2,19 +2,18 @@ package ddd.caffeine.ratrip.module.travel_plan.presentation.dto.request;
 
 import java.time.LocalDate;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import ddd.caffeine.ratrip.module.place.domain.Region;
 import ddd.caffeine.ratrip.module.travel_plan.application.dto.CreateTravelPlanDto;
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateTravelPlanRequestDto {
-	@NotEmpty(message = "Region must not be Blank")
 	private Region region;
 
 	private LocalDate travelStartDate;
