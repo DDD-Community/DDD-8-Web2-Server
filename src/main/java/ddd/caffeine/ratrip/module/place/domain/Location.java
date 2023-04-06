85b000fe6cc6033b08fdde1da3aa254de96f234e
jihwan <sgh002400@naver.com>
2023-04-07 01:40:17 +0900

    리팩토링

--- src/main/java/ddd/caffeine/ratrip/module/place/domain/Location.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/domain/Location.java
@@ -1,17 +1,17 @@
 package ddd.caffeine.ratrip.module.place.domain;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
 @NoArgsConstructor
+@AllArgsConstructor
 public class Location {
 	@NotNull
 	@Column
@@ -21,28 +21,7 @@ public class Location {
 	@Column
 	private double longitude;
 
-	private Location(double longitude, double latitude) {
-		this.longitude = longitude;
-		this.latitude = latitude;
-	}
-
 	public static Location of(String longitude, String latitude) {
 		return new Location(Double.parseDouble(latitude), Double.parseDouble(longitude));
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Location location = (Location)o;
-		return Double.compare(location.latitude, latitude) == 0
-			&& Double.compare(location.longitude, longitude) == 0;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(latitude, longitude);
 	}
 }
