da14162e9829219b445cfc019bcb677be819ca20
jihwan <sgh002400@naver.com>
2023-04-06 19:55:55 +0900

    리팩토링

--- src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/request/RecommendByLocationRequestDto.java
+++ src/main/java/ddd/caffeine/ratrip/module/place/presentation/dto/request/RecommendByLocationRequestDto.java
@@ -3,9 +3,11 @@ package ddd.caffeine.ratrip.module.place.presentation.dto.request;
 import javax.validation.constraints.NotNull;
 
 import ddd.caffeine.ratrip.module.place.application.dto.RecommendByLocationDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class RecommendByLocationRequestDto {
 	@NotNull(message = "Latitude must not be null")
 	private Double latitude;
